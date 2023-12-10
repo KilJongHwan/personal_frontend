95cbda0e61ed7141b9547b785fa7cef0141b3e9b
JH <dafr47@naver.com>
2023-12-10 18:03:01 +0900

    css 수정

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,4 +1,11 @@
+import { ReactComponent as Heart } from "../images/HeartBox.svg";
+import { ReactComponent as Subs } from "../images/SubscriberBox.svg";
+
 import {
+  Artist,
+  ArtistContainer,
+  InterBox,
+  InterBoxText,
   MainHead,
   MainHeadBox,
   MainHeadText,
@@ -13,11 +20,29 @@ const MyPage = () => {
       <MyPageContainer>
         <MainHead>
           <MainProfile></MainProfile>
-          <MainHeadBox>
-            <MainHeadText>공연 횟수 : 8</MainHeadText>
-            <MainHeadText>등록한 곡 : 6</MainHeadText>
-          </MainHeadBox>
-          <MainHeadText>가입일 : 2022-04-01</MainHeadText>
+          <ArtistContainer>
+            <MainHeadBox>
+              <MainHeadText>공연 횟수 : 1</MainHeadText>
+              <MainHeadText>등록한 곡 : 6</MainHeadText>
+              <InterBox>
+                <InterBoxText>
+                  <Heart />
+                  1000
+                </InterBoxText>
+              </InterBox>
+              <InterBox>
+                <InterBoxText>
+                  <Subs />
+                  1000
+                </InterBoxText>
+              </InterBox>
+            </MainHeadBox>
+
+            <Artist>
+              <MainHeadText>가입일 : 2022-04-01</MainHeadText>
+              ARTIST
+            </Artist>
+          </ArtistContainer>
         </MainHead>
         <NameText>독산동 인디언</NameText>
       </MyPageContainer>
--- src/style/MyPageStyle.jsx
+++ src/style/MyPageStyle.jsx
@@ -4,40 +4,43 @@ import profile from "../images/ProFileimage.png";
 export const MyPageContainer = styled.div`
   width: 1920px;
   height: 2608px;
-  opacity: var(--, 1);
+  opacity: 1;
 
   background: #fff;
 `;
 export const MainHead = styled.div`
   display: flex;
-
-  width: 1920px;
-  height: 356px;
+  width: 100%;
+  height: 24em;
   flex-shrink: 0;
   background: linear-gradient(98deg, #008bff 3.66%, #61e6ca 97.99%);
   overflow: hidden;
 `;
 export const MainProfile = styled.div`
   position: relative;
-  width: 281px;
-  height: 281px;
+  width: 20em;
+  height: 20em;
   flex-shrink: 0;
-  border-radius: 281px;
+  border-radius: 20em;
   border: 50px solid #fff;
-  opacity: var(--, 1);
+  opacity: 1;
   background: url(${profile}) no-repeat center / contain, lightgray;
-  margin-top: 30px;
-  margin-left: 70px;
+  margin-top: 1.5em;
+  margin-left: 5em;
 `;
 export const MainHeadBox = styled.div`
-  width: 389px;
-  height: 122px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35em;
+  height: 10em;
   flex-shrink: 0;
+  flex-direction: row;
 `;
 export const MainHeadText = styled.div`
   display: flex;
   position: relative;
-  width: 20rem;
+  width: Auto;
   height: 2rem;
   margin: 2rem;
   flex-direction: column;
@@ -46,21 +49,76 @@ export const MainHeadText = styled.div`
   flex-shrink: 0;
   color: #fff;
 
-  opacity: var(--, 1);
+  opacity: 1;
   color: #fff;
   font-family: Noto Sans KR;
-  font-size: 20px;
+  font-size: 1.5rem;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
 `;
+export const ArtistContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+  height: 100%;
+`;
+export const Artist = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 4em;
+  height: 1.5em;
+  flex-shrink: 0;
+  color: #fff;
+
+  font-family: Noto Sans KR;
+  font-size: 10em;
+  font-style: normal;
+  font-weight: 100;
+  line-height: 0.85;
+`;
+
 export const NameText = styled.div`
   position: relative;
   color: #000;
-
+  width: 10em;
   text-align: center;
   font-family: Noto Sans KR;
-  font-size: 40px;
+  font-size: 5em;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+export const InterBox = styled.div`
+  display: flex;
+  position: relative;
+  width: 6em;
+  height: 3.3em;
+  flex-shrink: 0;
+  border-radius: 10em;
+  opacity: 1;
+  margin: 1.5em;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  background: #fff;
+`;
+export const InterBoxText = styled.div`
+  display: flex;
+  width: 3em;
+  height: 1.5emx;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  flex: 1;
+  flex-shrink: 0;
+  color: #000;
+
+  font-family: Noto Sans KR;
+  font-size: 20px;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
