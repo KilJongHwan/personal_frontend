6440a366be98d320f3b04a176f753f27cfaaaf84
JH <dafr47@naver.com>
2023-12-24 13:25:41 +0900

    스트림 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { SuccessPage } from "./conponent/Mypage/SuccessComponent";
+import StreamPlayer from "./conponent/stream/StreamPlayer";
 
 import CommunityPage from "./pages/CommunityPage";
 import MyPage from "./pages/MyPage";
@@ -16,6 +17,7 @@ function App() {
           {/* 중첩 라우터로 커뮤니티 페이지안에 라우트를 연결해서 페이지안 컴포넌트만 변경 */}
           <Route path="/community/*" element={<CommunityPage />} />
           <Route path="/" element={<MyPage />} />
+          <Route path="/stream" element={<StreamPlayer />} />
         </Routes>
       </Router>
     </>
--- src/conponent/stream/StreamPlayer.jsx
+++ src/conponent/stream/StreamPlayer.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import OBSWebSocket from "obs-websocket-js";
 
 const obs = new OBSWebSocket();
 
 const StreamPlayer = () => {
-  const playerRef = useRef(null);
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
     // OBS 웹소켓에 연결합니다.
     obs
       .connect({
-        address: "localhost:4444", // OBS 웹소켓 서버 주소
-        password: "yourpassword", // OBS 웹소켓 비밀번호
+        address: "localhost:4465", // OBS 웹소켓 서버 주소
+        password: "abc123", // OBS 웹소켓 비밀번호
       })
       .then(() => {
         console.log("Success! Connected to OBS.");
@@ -24,26 +24,21 @@ const StreamPlayer = () => {
 
   const startStream = async () => {
     // OBS에서 스트리밍을 시작합니다.
-    obs
-      .send("StartStreaming")
-      .then(() => {
-        console.log("Streaming started");
-      })
-      .catch((err) => {
-        console.log("Error starting stream: ", err);
-      });
+    try {
+      await obs.send("StartStreaming");
+      console.log("Streaming started");
+    } catch (err) {
+      console.log("Error starting stream: ", err);
+    }
 
     // 이 예제에서는 임시로 스트림 URL을 설정하였습니다.
     const streamUrl = "http://example.com/live/stream.m3u8";
-
-    if (playerRef.current) {
-      playerRef.current.load(streamUrl, true);
-    }
+    setUrl(streamUrl);
   };
 
   return (
     <div>
-      <ReactPlayer ref={playerRef} controls url="" />
+      <ReactPlayer controls url={url} />
       <button onClick={startStream}>Start Streaming</button>
     </div>
   );
