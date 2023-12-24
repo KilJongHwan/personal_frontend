21b4ca95cc0a2e3070afc911fa89f8f7a6928e81
JH <dafr47@naver.com>
2023-12-24 19:56:40 +0900

    스트리밍 진행중

--- src/conponent/stream/StreamPlayer.jsx
+++ src/conponent/stream/StreamPlayer.jsx
@@ -1,38 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactPlayer from "react-player";
 import OBSWebSocket from "obs-websocket-js";
-
-const obs = new OBSWebSocket();
+import Common from "../../utils/Common";
 
 const StreamPlayer = () => {
   const [url, setUrl] = useState("");
+  const obs = useRef(new OBSWebSocket()); // useRef를 사용하여 obs 인스턴스 관리
 
   useEffect(() => {
-    // OBS 웹소켓에 연결합니다.
-    obs
-      .connect({
-        address: "localhost:4465", // OBS 웹소켓 서버 주소
-        password: "abc123", // OBS 웹소켓 비밀번호
-      })
-      .then(() => {
-        console.log("Success! Connected to OBS.");
-      })
-      .catch((err) => {
-        console.log("Connection failed: ", err);
-      });
+    connectToObs();
   }, []);
 
+  const connectToObs = async () => {
+    try {
+      await obs.current.connect("localhost:4444", "6OwgeXx7Fdu22Bgr");
+
+      console.log("Success! Connected to OBS.");
+      console.log(obs.current);
+    } catch (err) {
+      console.log("Connection failed: ", err);
+    }
+  };
+
   const startStream = async () => {
+    if (!obs.current) {
+      console.log("OBS connection not established yet.");
+      return;
+    }
+
     // OBS에서 스트리밍을 시작합니다.
     try {
-      await obs.send("StartStreaming");
-      console.log("Streaming started");
+      await obs.current.send("StartStreaming");
+      // ... 나머지 코드 생략 ...
     } catch (err) {
       console.log("Error starting stream: ", err);
     }
 
     // 이 예제에서는 임시로 스트림 URL을 설정하였습니다.
-    const streamUrl = "http://example.com/live/stream.m3u8";
+    const streamUrl = Common.DOMAIN + `/stream`;
     setUrl(streamUrl);
   };
 
